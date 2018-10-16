e99e23a3a9c5e84c93bd70b03e8fae62fba29980
Sajid Moinuddin <sajid.moinuddin@foxsports.com.au>
2018-10-17 10:02:35 +1100

    fixed jx location

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SA=~/.gke_sa.json
 HELM_VERSION=2.10.0
 
 function install_dependencies() {
-	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
+	wget https://github.com/jenkins-x/jx/releases/download/${JX_VERSION}/jx-linux-amd64.tar.gz
 	tar xvf jx-linux-amd64.tar.gz
 	rm jx-linux-amd64.tar.gz
 
